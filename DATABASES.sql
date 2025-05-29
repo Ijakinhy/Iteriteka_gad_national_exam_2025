6ad71972c4172a3bbe788c60069c57bec7f07d31
jakin <ijakinhy@gmail.com>
2025-05-29 10:58:22 +0200

    fix error

--- DATABASES.sql
+++ DATABASES.sql
@@ -1,49 +1,46 @@
 -- Create the database
 CREATE DATABASE IF NOT EXISTS CRPMS;
-
--- Use the database
 USE CRPMS;
 
-
+-- 1. Create Users Table 
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     Username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
-
 );
 
--- 1. Create Services Table
-CREATE TABLE Services (
-    ServiceCode INT(10) AUTO_INCREMENT PRIMARY KEY,
-    ServiceName VARCHAR(100),
-    ServicePrice DECIMAL(10,2)
+-- 2. Create Services Table 
+CREATE TABLE  Services (
+    ServiceCode INT AUTO_INCREMENT PRIMARY KEY,
+    ServiceName VARCHAR(100) NOT NULL,
+    ServicePrice DECIMAL(10,2) NOT NULL
 );
 
--- 2. Create Car Table
-CREATE TABLE Car (
-    PlateNumber INT(15) AUTO_INCREMENT PRIMARY KEY,
-    Type VARCHAR(50),
-    Model VARCHAR(50),
-    ManufacturingYear YEAR,
+-- 3. Create Car Table 
+CREATE TABLE  Car (
+    PlateNumber INT AUTO_INCREMENT PRIMARY KEY,
+    Type VARCHAR(50) NOT NULL,
+    Model VARCHAR(50) NOT NULL,
+    ManufacturingYear YEAR NOT NULL,
     DriverPhone VARCHAR(20),
     MechanicName VARCHAR(100)
 );
 
--- 3. Create ServiceRecord Table
-CREATE TABLE ServiceRecord (
+-- 4. Create ServiceRecord Table 
+CREATE TABLE  ServiceRecord (
     RecordNumber INT AUTO_INCREMENT PRIMARY KEY,
-    PlateNumber INT(15),
-    ServiceCode INT(10),
-    ServiceDate DATE,
+    PlateNumber INT NOT NULL,
+    ServiceCode INT NOT NULL,
+    ServiceDate DATE NOT NULL,
     FOREIGN KEY (PlateNumber) REFERENCES Car(PlateNumber),
     FOREIGN KEY (ServiceCode) REFERENCES Services(ServiceCode)
 );
 
--- 4. Create Payment Table
-CREATE TABLE Payment (
+-- 5. Create Payment Table 
+CREATE TABLE  Payment (
     PaymentNumber INT AUTO_INCREMENT PRIMARY KEY,
-    RecordNumber INT,
-    AmountPaid DECIMAL(10,2),
-    PaymentDate DATE,
+    RecordNumber INT NOT NULL,
+    AmountPaid DECIMAL(10,2) NOT NULL,
+    PaymentDate DATE NOT NULL,
     FOREIGN KEY (RecordNumber) REFERENCES ServiceRecord(RecordNumber)
 );
